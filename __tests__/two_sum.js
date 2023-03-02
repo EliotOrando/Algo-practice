f8d29ed4e2967eaf9c3bf825f3767d9e9de0dc2c
EliotOrando <eorando@gmail.com>
2023-03-02 08:41:42 -0800

    committing solutions branch to push to github

--- __tests__/two_sum.js
+++ __tests__/two_sum.js
@@ -7,9 +7,9 @@ describe('TwoSum tests', () => {
   });
   it('should handle cases where there is no solution', () => {
     expect(twoSum([1, 6, 2, 7], 5)).toBe(-1);
-  })
+  });
   it('should find the indicies of values that add up to target', () => {
     expect(JSON.stringify(twoSum([1, 2, 3, 5], 5))).toBe('[1,2]');
     expect(JSON.stringify(twoSum([10, 15, 20, 4, 1], 5))).toBe('[3,4]');
-  })
-})
+  });
+});
