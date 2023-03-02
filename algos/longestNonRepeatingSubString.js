93b528a0e09024a6f8ebbb083842dc4f2834ece0
EliotOrando <eorando@gmail.com>
2023-03-02 09:00:36 -0800

    completed longestNonRepeating and two sum

--- algos/longestNonRepeatingSubString.js
+++ algos/longestNonRepeatingSubString.js
@@ -17,8 +17,31 @@ Explanation: The answer is "wke", with the length of 3.
 Notice that the answer must be a substring, "pwke" is a subsequence and not a substring. */
 
 const longestNonRepeatingSubString = (s) => {
-
-  
+  //creating a set to hold the characters in my substring
+  const chars = new Set();
+  //create longest variable to find the length of my nonRepeating substring
+  let longest = 0;
+  //initialize i and j (two pointers moving through my string)
+  let i = 0, j = 0;
+  //iterate over the string
+  while(j < s.length){
+      //This conditional checks to see if the set has the character we are currently looking at s[j]
+      if(chars.has(s[j])){
+        //if so, we delete s[i] (NOT s[j]) from the set, and increment i.
+        chars.delete(s[i]);
+        i++;
+      }
+      else{
+        //else we don't have this character yet, so we add the character ingo the set
+        chars.add(s[j]);
+        //reassign longest to the max of longest and our set's size
+        longest = Math.max(longest, chars.size);
+        //and increment j
+        j++;
+      }
+  }
+  //return the length of the longest substring when we're done with our loop. 
+  return longest;
 };
 
 module.exports = {longestNonRepeatingSubString}
--- algos/two_sum.js
+++ algos/two_sum.js
@@ -15,7 +15,19 @@
 */
 
 const twoSum = (nums, target) => {
-  
+  //object map holding properties where key = difference and value = index
+  const map = {};
+  for(let i = 0; i < nums.length; i++){
+      //check to see if our number currently exists in the map. if so return an array of the index in the map and the current index
+      if(map[nums[i]] || map[nums[i]] === 0) return [map[nums[i]], i];
+      else {
+        //else we calculate the difference, and then put that into our map object with key being difference and value being the index at which this value is found. 
+          let diff = target - nums[i];
+          map[diff] = i;
+      }
+  }
+  //return -1 if we don't find a solution. 
+  return -1;
 };
 
 module.exports = {twoSum}
