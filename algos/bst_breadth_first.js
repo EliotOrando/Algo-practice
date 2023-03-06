3338fde6d791a13c685dd6ab1e89a5de5b8e0293
EliotOrando <eorando@gmail.com>
2023-03-06 09:03:31 -0800

    need to still add in the tests for bst_breadth_first on branch algos-tests-written

---
+++ algos/bst_breadth_first.js
@@ -0,0 +1,42 @@
+function BinarySearchTree(value) {
+  this.value = value;
+  this.right = null;
+  this.left = null;
+}
+
+/*
+
+Given the root of a binary search tree and a callback function, apply the
+callback to the values of the BST in breadth-first order.
+
+Example:
+
+If the tree is
+
+     4
+   /   \
+  2     7
+ / \     \
+1   3     9
+         /
+        8
+
+then apply the callback on the numbers in the order:
+4, 2, 7, 1, 3, 9, 8.
+
+Hint:
+
+Maintain a queue (array) consisting of the nodes we need to operate on.
+For each node in the queue, push the left and right children (if applicable) to
+the end of the queue. Keep consuming the queue (using the shift method) until
+there are no more nodes in the queue.
+
+Utilizing recursion is not necessary, nor recommended.
+
+*/
+
+function bst(root, callback){
+
+}
+
+module.exports = {bst};
