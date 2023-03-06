f7df4068d4782364faf158239507a03bc1d3bb21
EliotOrando <eorando@gmail.com>
2023-03-06 08:45:12 -0800

    completed the branch for container_with_most_water problem

---
+++ algos/container_with_most_water.js
@@ -0,0 +1,30 @@
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Picture: https://dev.to/alisabaj/the-container-with-the-most-water-solving-an-algorithm-about-areas-kkl
+
+Example: 
+height: [1, 3, 2, 5]
+return value of 6 b/c maximum area made by the heights is a rectangle of height 3, with a width of 2. (the rectangle formed by connecting the 3 and the 5 heights) - see example graph
+
+*****Example graph******
+^
+|
+|               |  
+|        _______|
+|       |       |
+|       |   |   | 
+|   |   |   |   |
+------------------------>
+    1   3   2   5
+*/
+
+function maxArea (height) {
+  
+}
+
+module.exports = {maxArea}
