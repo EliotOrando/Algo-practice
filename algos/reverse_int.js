0004242e02622ca901fd7d5f81081d18ac7b3ce0
EliotOrando <eorando@gmail.com>
2023-03-07 09:57:02 -0800

    added in the reverse_int algo file on the main branch

---
+++ algos/reverse_int.js
@@ -0,0 +1,16 @@
+/*
+Given an integer x, return x with it's digits reversed. 
+
+i.e. 
+
+reverseInt(592) --> 295
+
+reverseInt(-12) --> -21
+*/
+
+function reverseInt(x){
+
+
+};
+
+module.exports = {reverseInt};
