241f894fe4272bbb1907a3bca6bd437d2087ce73
EliotOrando <eorando@gmail.com>
2023-03-08 12:57:08 -0800

    finished setting up tests for zigzag conversion

--- __tests__/zigzag_conversion.js
+++ __tests__/zigzag_conversion.js
@@ -1,13 +1,14 @@
-const convert = require('../algos/zigzag_conversion');
+const {convert} = require('../algos/zigzag_conversion');
 
 describe('zigzag converstion tests:', () => {
   it('should handle num rows of 1', () => {
     expect(convert('A', 1)).toBe('A');
     expect(convert('ABCDEFG', 1)).toBe('ABCDEFG');
   });
-  // it('should handle num rows of 2', () => {
-
-  // });
+  it('should handle num rows of 2', () => {
+    expect(convert('TESTSTRING', 2)).toBe('TSSRNETTIG');
+    expect(convert('TESTINGISFUN', 2)).toBe('TSIGSUETNIFN');
+  });
   it('should handle num rows of various lengths', () => {
     expect(convert('PAYPALISHIRING', 3)).toBe('PAHNAPLSIIGYIR');
     expect(convert('PAYPALISHIRING', 4)).toBe('PINALSIGYAHRPI');
