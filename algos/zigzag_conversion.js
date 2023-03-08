92892109bea0984330b28c2d1bb0e5c180588d48
EliotOrando <eorando@gmail.com>
2023-03-08 12:47:24 -0800

    files setup for zigzag converstion

---
+++ algos/zigzag_conversion.js
@@ -0,0 +1,22 @@
+/* 
+Leetcode problem 6:
+
+Write the code that will take a string and make a conversion given a number of rows --> string convert(string s, int numRows);
+
+The conversion pattern follows the zigzag pattern going down first, then diagonally up and to the right. 
+
+i.e. convert("PAYPALISHIRING", 4) --> "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+
+*/
+
+const convert = (s, numRows) => {
+
+};
+
+module.exports = {convert};
