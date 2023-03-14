84afad42be44d55eef566708bb4fce7ab62fa668
EliotOrando <eorando@gmail.com>
2023-03-14 09:20:57 -0700

    completed the test file and the shell of algo file for bin-to-dec in algos-test-written branch

---
+++ algos/bin-to-dec.js
@@ -0,0 +1,31 @@
+/* 
+
+Given a string that represents a Binary Number, write a function that converts this string into a decimal number. DO NOT use the native parseInt() method. 
+
+For example: 
+
+binToDec('0')   -> 0
+binToDec('11')  -> 3
+binToDec('100') -> 4
+binToDec('101') -> 5
+binToDec('0101') -> 5
+
+1 -> 1
+10 -> 2
+100 -> 4
+1000 -> 8
+10000 -> 16
+
+
+For more information on how to read binary, check out this article: 
+https://m.wikihow.com/Read-Binary
+
+*/
+
+function binToDec(binString){
+ 
+}
+
+
+
+module.exports = {binToDec};
