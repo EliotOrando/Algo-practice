a13d97f77839d605e20c3e2b4b53df924a4b6f80
EliotOrando <eorando@gmail.com>
2023-03-15 10:20:01 -0700

    removed the solution and the written tests for roman num to int on main branch. Now it's just the outline- ready to push all 3 branches to github

--- algos/roman-num-to-int.js
+++ algos/roman-num-to-int.js
@@ -28,46 +28,9 @@ Given a roman numeral, convert it to an integer.
 
 //iterative solution
 
-const romanValue = {
-  'I' : 1,
-  'V' : 5,
-  'X' : 10,
-  'L' : 50,
-  'C' : 100,
-  'D' : 500,
-  'M' : 1000,
-}
-
 const romanToInt = (string) => {
-  let total = 0;
-    for(let i = 0; i < string.length -1 ; i++){
-        let currVal = romanValue[string[i]], nextVal = romanValue[string[i+1]];
-        currVal < nextVal ? total -= currVal : total += currVal;
-    }
-
-    return total + romanValue[string[string.length-1]];
+  
 }
 
-//recursive solution (less optimal in both time and space. )
-
-/* const romanToInt = (string) => {
-  const exception = {
-    'IV' : 4,
-    'IX' : 9,
-    'XL' : 40,
-    'XC' : 90,
-    'CD' : 400,
-    'CM' : 900,
-  }
-
-  if(string.length === 0) return 0;
-  //alternate base case is that string length is 1
-  if(string.length === 1) return romanValue[string];
-  //recurse case
-  //1. first check to make sure the first char isn't an exception 
-  if(exception[string.slice(0,2)]) return exception[string.slice(0,2)] + romanToInt(string.slice(2));
-  //2. return the curent value plus the rest of the string if it is not an exception
-  else return romanValue[string[0]] + romanToInt(string.slice(1));
-} */
 
 module.exports = { romanToInt };
