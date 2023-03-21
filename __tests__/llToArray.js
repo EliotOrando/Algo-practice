b11bf6505724bcc2b82ed9ad2e3ef528a0e669b2
EliotOrando <eorando@gmail.com>
2023-03-21 09:49:25 -0700

    corrected a mistake in the test file for llToArray

--- __tests__/llToArray.js
+++ __tests__/llToArray.js
@@ -1,15 +1,21 @@
-/* 
-Write a function that takes in the head node of a linked list, and returns out an array of all the values. 
-*/
+const {LinkedList, Node} = require('../linkedList');
+const {llToArray} = require('../algos/llToArray');
 
-const llToArray = (node) => {
-  const returnArr = [];
-  while(node){
-    returnArr.push(node.val);
-    node = node.next;
-  }
-  return returnArr;
-}
-
-
-module.exports = {llToArray};
+describe('linked list to array tests:', () => {
+  const ll = new LinkedList();
+  it('should handle linked lists of various lengths', () => {
+    ll.head = new Node(1);
+    ll.head.next = new Node(2);
+    ll.head.next.next = new Node (3);
+    expect(llToArray(ll.head)).toEqual([1, 2, 3]);
+    ll.head = new Node('hi');
+    ll.head.next = new Node('12345');
+    ll.head.next.next = new Node(false);
+    ll.head.next.next.next = new Node(45);
+    expect(llToArray(ll.head)).toEqual(['hi', '12345', false, 45])
+  });
+  it('should handle empty linked lists', () => {
+    ll.head = null;
+    expect(llToArray(ll.head)).toEqual([]);
+  });
+})
