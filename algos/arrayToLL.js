1bd81a7fcb4bb51a34a65ed7d468e282aed55fd7
EliotOrando <eorando@gmail.com>
2023-03-21 09:46:20 -0700

    need to fix a mistake in test repo. Commmitting to switch branches and fix mistake

---
+++ algos/arrayToLL.js
@@ -0,0 +1,10 @@
+/* 
+Write an algorithm that converts an array to a linked list. Return the head node. 
+*/
+const { Node } = require('../linkedList')
+
+const arrayToLL = (arr) => {
+  
+}
+
+module.exports = { arrayToLL }
