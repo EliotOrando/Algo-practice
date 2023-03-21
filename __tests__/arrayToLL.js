bf2b8b1b1fbcea8b535ffa82b76fb9ee9aa57092
EliotOrando <eorando@gmail.com>
2023-03-21 09:46:49 -0700

    corrected mistake - test file is good now

--- __tests__/arrayToLL.js
+++ __tests__/arrayToLL.js
@@ -1,10 +1,15 @@
-/* 
-Write an algorithm that converts an array to a linked list. Return the head node. 
-*/
-const { Node } = require('../linkedList')
+const { arrayToLL } = require('../algos/arrayToLL');
+const { llToArray } = require('../algos/llToArray');
 
-const arrayToLL = (arr) => {
-  
-}
-
-module.exports = { arrayToLL }
+describe('array to linked list tests:', () => {
+  const arr1 = [1, 2, 3, 4, 5];
+  const arr2 = ['hi', '12345', false, 45];
+  const arr3 = [];
+  it('should handle empty arrays', () => {
+    expect(llToArray(arrayToLL(arr3))).toEqual(arr3);
+  });
+  it('should handle array\'s of various lengths', () => {
+    expect(llToArray(arrayToLL(arr2))).toEqual(arr2);
+    expect(llToArray(arrayToLL(arr1))).toEqual(arr1);
+  });
+});
