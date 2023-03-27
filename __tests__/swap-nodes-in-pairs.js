50bb0efedd70875e46c8589886c78570cddb1afd
EliotOrando <eorando@gmail.com>
2023-03-27 08:52:58 -0700

    finished setting up skeleton files for swap-nodes-in-pairs.js |

--- __tests__/swap-nodes-in-pairs.js
+++ __tests__/swap-nodes-in-pairs.js
@@ -1,6 +1,8 @@
-const {LinkedList, Node} = require('../linkedList');
+const { LinkedList, Node } = require('../linkedList');
+const { arrayToLL } = require('../algos/arrayToLL');
+const { llToArray } = require('../algos/llToArray');
 const { swapNodesInPairs } = require('../algos/swap-nodes-in-pairs');
 
 describe('swap nodes in pairs tests:', () => {
-
+ 
 });
