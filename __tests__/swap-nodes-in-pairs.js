b9f6c5a60c049004850a5eebda0eaa492b4c3cba
EliotOrando <eorando@gmail.com>
2023-03-27 08:40:34 -0700

    added skeleton files for swap-nodes-in-pars (both algo file and test file). Ready to be solved and have tests written

---
+++ __tests__/swap-nodes-in-pairs.js
@@ -0,0 +1,6 @@
+const {LinkedList, Node} = require('../linkedList');
+const { swapNodesInPairs } = require('../algos/swap-nodes-in-pairs');
+
+describe('swap nodes in pairs tests:', () => {
+
+});
---
+++ algos/swap-nodes-in-pairs.js
@@ -0,0 +1,30 @@
+/*
+Leetcode problem 24 - Medium
+
+Given a linked list, swap every two adjacent nodes and return its head. 
+You must solve the problem without modifying the values in the list's nodes 
+(i.e., only nodes themselves may be changed.)
+
+Example 1:
+
+
+Input: head = 1 > 2 > 3 > 4 > null
+Output: 2 > 1 > 4 > 3 > null
+
+Example 2:
+
+Input: head = null
+Output: null
+
+Example 3:
+
+Input: head = 1 > null
+Output: 1 > null
+
+*/
+
+const swapNodesInPairs = (head) => {
+
+}
+
+module.exports = { swapNodesInPairs }
