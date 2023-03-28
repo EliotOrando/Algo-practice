c991462a8a254fd25f32a74e8374fb7c2ecb4a9c
EliotOrando <eorando@gmail.com>
2023-03-28 12:10:03 -0700

    finished test file and skeleton algo for generate-parentheses

---
+++ algos/generate-parentheses.js
@@ -0,0 +1,20 @@
+/*
+Leetcode problem 23
+
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Example 1:
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+
+Example 2:
+Input: n = 1
+Output: ["()"]
+
+*/
+
+const generateParentheses = (n) => {
+
+}
+
+module.exports = { generateParentheses }
