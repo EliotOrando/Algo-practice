62019d8cb6ffbc2e5835f3e4bbf39b7fdf42306a
EliotOrando <eorando@gmail.com>
2023-04-10 09:52:20 -0700

    deleteDuplicates skeleton test and algo file completed

---
+++ __tests__/deleteDuplicates.js
@@ -0,0 +1,7 @@
+const {arrayToLL} = require('../algos/arrayToLL');
+const {llToArray} = require('../algos/llToArray');
+const {deleteDuplicates} = require('../algos/deleteDuplicates');
+
+describe('deleteDuplicates tests:', () => {  
+  
+});
